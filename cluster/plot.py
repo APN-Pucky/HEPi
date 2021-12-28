460dbf3f6f9dbe76d5b6225d7214e1a5ff0c6bf1
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-12-28 21:42:31 +0100

    Split run and plot

--- cluster/plot.py
+++ cluster/plot.py
@@ -13,10 +13,17 @@ input.set_pre(
     "srun --nodes 1 --cpus-per-task 1 --partition normal --mail-type=ALL --time=06:00:00 --mail-user=a_neuw01@uni-muenster.de")
 print(rs.get_path())
 
-
 for pdf in ["CT14lo"]:
     for p in [2000002, 1000002]:
         i = hepi.Input(hepi.Order.NLO, 7000, p, 1000022,
                        "sps1a1000.in", pdf, pdf, 1., 1.)
         li = hepi.mass_scan([i], p, np.linspace(300, 950, 16), diff_L_R=20)
-        dll = rs.run(li, True, False, True)
+        dll = rs.run(li, False, False, False)
+        hepi.mass_plot(dll, p, "K_lo", logy=False, label="lo")
+        # hepi.mass_vplot(dll,p,((dll["vnlo"]+dll["p_plus_k"]+dll["lo"])/dll["lo"]),logy=False,label="vnlo+P+K")
+        # hepi.mass_vplot(dll,p,((dll["rnlo"]+dll["lo"])/dll["lo"]),logy=False,label="rnlo")
+
+        hepi.mass_plot(dll, p, "K_nlo", logy=False,
+                       yaxis="$K_\\mathrm{nlo}$", label="nlo")
+
+        plt.savefig(input.get_output_dir() + "test" + str(p) + ".pdf")
