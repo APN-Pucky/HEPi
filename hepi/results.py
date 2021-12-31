aa1567c191fc762a53dfac94f102558423005333
APN-Pucky <APN-Pucky@users.noreply.github.com>
2021-12-31 17:09:25 +0100

    intermediate add pdf error calc

--- hepi/results.py
+++ hepi/results.py
@@ -25,3 +25,25 @@ class ResultWithError(Result):
                  nlo_plus_nll, nlo_plus_nll_up_scale, nlo_plus_nll_down_scale, nlo_plus_nll_up_pdf, nlo_plus_nll_down_pdf,
                  ):
         Result.__init__(self, lo, nlo, nlo_plus_nll)
+
+
+def pdf_error(li, dl):
+    example = li[0]
+    members = [attr for attr in dir(example) if not callable(
+        getattr(example, attr)) and not attr.startswith("__")]
+    for i in range(len(dl["pdfset_nlo"])):
+        if dl["pdfset_nlo"][i] == 0:
+            set = lhapdf.getPDFSet(dl["pdf_nlo"][i])
+            pdfs = [None] * set.size()
+            for j in range(len(dl["pdfset_nlo"])):
+                same = True
+                for s in members:
+                    if not (dl[s][i] == dl[s][j] and s != "pdfset_nlo"):
+                        same = False
+                if same:
+                    pdfs[dl["pdfset_nlo"][j]] = j
+
+            lo_unc = set.uncertainty([dl["lo"][k] for k in pdfs], -1)
+            nlo_unc = set.uncertainty([dl["nlo"][k] for k in pdfs], -1)
+            nlo_plus_nll_unc = set.uncertainty(
+                [dl["nlo_plus_nll"][k] for k in pdfs], -1)
