aef37af52a20a855c317f869cc9a9a8b0840be63
Alexander Neuwirth <a_neuw01@uni-muenster.de>
2021-12-31 13:17:52 +0100

    smll ch

--- cluster/plot.py
+++ cluster/plot.py
@@ -6,16 +6,16 @@ import hepi.input as input
 import numpy as np
 import matplotlib.pyplot as plt
 
-rs.set_path("~/git/resummino/")
-input.set_output_dir("/scratch/tmp/a_neuw01/hepi")
-input.set_input_dir("~/git/hepi/tests/input/")
+rs.set_path("/home/a/a_neuw01/git/resummino/")
+input.set_output_dir("/scratch/tmp/a_neuw01/hepi/")
+input.set_input_dir("/home/a/a_neuw01/git/hepi/tests/input/")
 input.set_pre(
-    "srun --nodes 1 --cpus-per-task 1 --partition normal --mail-type=ALL --time=06:00:00 --mail-user=a_neuw01@uni-muenster.de")
+    "srun --nodes 1 --partition normal --mail-type=ALL --time=06:00:00 --mail-user=a_neuw01@uni-muenster.de")
 print(rs.get_path())
 
 for pdf in ["CT14lo"]:
-    for p in [2000002, 1000002]:
-        i = hepi.Input(hepi.Order.NLO, 7000, p, 1000022,
+    for p in [2000002, ]:
+        i = hepi.Input(hepi.Order.LO, 7000, p, 1000022,
                        "sps1a1000.in", pdf, pdf, 1., 1.)
         li = hepi.mass_scan([i], p, np.linspace(300, 950, 16), diff_L_R=20)
         dll = rs.run(li, False, False, False)
@@ -23,7 +23,7 @@ for pdf in ["CT14lo"]:
         # hepi.mass_vplot(dll,p,((dll["vnlo"]+dll["p_plus_k"]+dll["lo"])/dll["lo"]),logy=False,label="vnlo+P+K")
         # hepi.mass_vplot(dll,p,((dll["rnlo"]+dll["lo"])/dll["lo"]),logy=False,label="rnlo")
 
-        hepi.mass_plot(dll, p, "K_nlo", logy=False,
-                       yaxis="$K_\\mathrm{nlo}$", label="nlo")
+        #hepi.mass_plot(dll, p, "K_nlo", logy=False,
+        #               yaxis="$K_\\mathrm{nlo}$", label="nlo")
 
-        plt.savefig(input.get_output_dir() + "test" + str(p) + ".pdf")
+        plt.savefig(input.get_output_dir() + "testprec" + str(p) + ".pdf")
--- cluster/run.py
+++ cluster/run.py
@@ -10,13 +10,13 @@ rs.set_path("/home/a/a_neuw01/git/resummino/")
 input.set_output_dir("/scratch/tmp/a_neuw01/hepi/")
 input.set_input_dir("/home/a/a_neuw01/git/hepi/tests/input/")
 input.set_pre(
-    "srun --nodes 1 --cpus-per-task 1 --partition normal --mail-type=ALL --time=06:00:00 --mail-user=a_neuw01@uni-muenster.de")
+    "srun --nodes 1 -c 2 --partition normal --mail-type=ALL --time=03:00:00 --mail-user=a_neuw01@uni-muenster.de")
 print(rs.get_path())
 
 
 for pdf in ["CT14lo"]:
-    for p in [2000002, 1000002]:
-        i = hepi.Input(hepi.Order.NLO, 7000, p, 1000022,
+    for p in [2000002, ]:
+        i = hepi.Input(hepi.Order.LO, 7000, p, 1000022,
                        "sps1a1000.in", pdf, pdf, 1., 1.)
         li = hepi.mass_scan([i], p, np.linspace(300, 950, 16), diff_L_R=20)
         dll = rs.run(li, True, False, True)
