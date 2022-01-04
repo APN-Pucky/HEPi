c51b1e8c785255b2fc8dd1ca6fdfb8313edd7dbe
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-04 21:11:18 +0100

    fix brackets

--- cluster/run_scale.py
+++ cluster/run_scale.py
@@ -1,9 +1,9 @@
 import cluster
-from . import *
+from cluster import *
 
 for run_plot in [True ]:
     for scenario in ["scenarioA.in","scenarioB.in"]:
-        for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118", ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180"))]:
+        for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118"), ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")]:
             for p in [2000002, 1000002]:
                 li = [hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)]
 
