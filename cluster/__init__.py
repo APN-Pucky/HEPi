42b08ffea5a98c918e1149ad6a0ef3e93182c58d
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-07 22:00:13 +0100

    24h run time

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -32,7 +32,7 @@ rs.set_path("/home/a/"+user+"/git/resummino/")
 input.set_output_dir("/scratch/tmp/"+user+"/hepi/")
 input.set_input_dir("/home/a/"+user+"/git/hepi/tests/input/")
 input.set_pre("while [[ $(squeue -u " + user+ " | wc -l) -gt 1998 ]]; do sleep 60; done && sbatch --job-name=" +get_job_name() +
-" --ntasks-per-node 1 --cpus-per-task 1  --time=06:00:00 --mem=100M --partition normal" +
+" --ntasks-per-node 1 --cpus-per-task 1  --time=24:00:00 --mem=100M --partition normal" +
 " --mail-type=ALL --mail-user="+user+"@uni-muenster.de")
 print(rs.get_path())
 
