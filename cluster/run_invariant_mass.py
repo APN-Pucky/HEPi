bcb310c5c0690863801fac530d1798d9bbf5b3d6
Alexander Neuwirth <a_neuw01@uni-muenster.de>
2022-01-07 15:58:35 +0100

    pre pull

--- cluster/run_invariant_mass.py
+++ cluster/run_invariant_mass.py
@@ -2,7 +2,7 @@ import cluster
 from cluster import *
 
 #wait()
-for run_plot in [True, ]:
+for run_plot in [True,False ]:
     for scenario in ["scenarioA.in","scenarioB.in"]:
         for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118"), ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")]:
             for p in [2000002, 1000002]:
