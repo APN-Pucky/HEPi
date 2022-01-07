c2b5100200ba34baefca801cbfafd51b7797f8da
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-07 05:25:29 +0100

    ch run

--- cluster/run_mass_squark.py
+++ cluster/run_mass_squark.py
@@ -4,31 +4,32 @@ from smpl import plot
 
 #wait()
 for run_plot in [True,False ]:
-    for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118"), ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")]:
-        for p in [2000002, 1000002]:
-            i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, p, 1000022, "scenarioB.in", lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)
+    for scenario in ["scenarioB.in"]:
+        for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118"), ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")]:
+            for p in [2000002, 1000002]:
+                i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)
 
-            li = hepi.mass_scan([i], p, np.linspace(1000, 3000, 32), diff_L_R=100)
-            li = hepi.scale_scan(li)
+                li = hepi.mass_scan([i], p, np.linspace(1000, 3000, 32), diff_L_R=100)
+                li = hepi.scale_scan(li)
 
-            dl = rs.run(li, False, False, run_plot,False)
+                dl = rs.run(li, False, False, run_plot,False)
 
-            if not run_plot:
-                dl = hepi.scale_error(li,dl)
-                plot.data([],[],init=True)
-                hepi.mass_plot(dl,  "lo_scale",p,           logy=True,mask=dl["lo_scale"]!=np.array(None), label="lo")
-                hepi.mass_plot(dl,  "nlo_scale",p,          logy=True,mask=dl["lo_scale"]!=np.array(None), label="nlo")
-                hepi.mass_plot(dl,  "nlo_plus_nll_scale",p, logy=True,mask=dl["lo_scale"]!=np.array(None), label="nlo+nll")
+                if not run_plot:
+                    dl = hepi.scale_error(li,dl)
+                    plot.data([],[],init=True)
+                    hepi.mass_plot(dl,  "lo_scale",p,           logy=True,mask=dl["lo_scale"]!=np.array(None), label="lo")
+                    hepi.mass_plot(dl,  "nlo_scale",p,          logy=True,mask=dl["lo_scale"]!=np.array(None), label="nlo")
+                    hepi.mass_plot(dl,  "nlo_plus_nll_scale",p, logy=True,mask=dl["lo_scale"]!=np.array(None), label="nlo+nll")
 
-                plt.savefig(input.get_output_dir() + "mass_" + str(p) + "_" + str(nlo_pdf) + ".pdf")
+                    plt.savefig(input.get_output_dir() + "mass_" + str(p) + "_" + str(nlo_pdf) + ".pdf")
 
-                plot.data([],[],init=True)
-                hepi.mass_plot(dl,  "lo_scale",p,           logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="lo")
-                hepi.mass_plot(dl,  "nlo_scale",p,          logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="nlo")
-                hepi.mass_plot(dl,  "nlo_plus_nll_scale",p, logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="nlo+nll")
+                    plot.data([],[],init=True)
+                    hepi.mass_plot(dl,  "lo_scale",p,           logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="lo")
+                    hepi.mass_plot(dl,  "nlo_scale",p,          logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="nlo")
+                    hepi.mass_plot(dl,  "nlo_plus_nll_scale",p, logy=False,K=True,mask=dl["lo_scale"]!=np.array(None), label="nlo+nll")
 
-                plt.savefig(input.get_output_dir() + "Kmass_" + str(p) + "_" + str(nlo_pdf) + ".pdf")
+                    plt.savefig(input.get_output_dir() + "Kmass_" + str(p) + "_" + str(nlo_pdf) + ".pdf")
 
-                hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + "mass" + str(p)+  "_"+ str(nlo_pdf) + ".tex",pdf=False)
+                    hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + "mass" + str(p)+  "_"+ str(nlo_pdf) +"_"+ str(scenario)+ ".tex",pdf=False)
     wait()
 
--- cluster/run_plehn_mass.py
+++ cluster/run_plehn_mass.py
@@ -3,36 +3,36 @@ from cluster import *
 from smpl import plot
 
 for run_plot in [False]:
-    for lo_pdf,nlo_pdf in [("cteq6l1","cteq66")]:
-        for p in [2000002, 1000002]:
-            i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 7000, p, 1000022, "sps1a1000_mod.in", lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)
+    for scenario in ["sps1a1000_mod.in"]:
+        for lo_pdf,nlo_pdf in [("cteq6l1","cteq66")]:
+            for p in [2000002, 1000002]:
+                i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 7000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)
 
-            li = hepi.mass_scan([i], p, np.linspace(300, 950, 16), diff_L_R=20)
-            li = hepi.scale_scan(li)
-            li = hepi.pdf_scan(li)
+                li = hepi.mass_scan([i], p, np.linspace(300, 950, 16), diff_L_R=20)
+                li = hepi.scale_scan(li)
+                li = hepi.pdf_scan(li)
 
-            dl = rs.run(li, False, False, run_plot,False)
-
-
-            if not run_plot:
-                dl = hepi.pdf_error(li,dl)
-                dl = hepi.scale_error(li,dl)
-                dl = hepi.combine_errors(dl)
-
-                plot.data([],[],init=True)
-                hepi.combined_plot(hepi.mass_plot,dl,"lo",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
-                hepi.combined_plot(hepi.mass_plot,dl,"nlo",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
-                hepi.combined_plot(hepi.mass_plot,dl,"nlo_plus_nll",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
-       
-                plt.savefig(input.get_output_dir()+ "comp_" + nlo_pdf + "_" + str(p) + ".pdf")
+                dl = rs.run(li, False, False, run_plot,False)
 
 
-                plot.data([],[],init=True)
-                hepi.combined_plot(hepi.mass_plot,dl, "lo",p, logy=False,K=True, label="lo",interpolate=False)
-                hepi.combined_plot(hepi.mass_plot,dl, "nlo",p, logy=False,K=True, label="nlo",interpolate=False)
-                hepi.combined_plot(hepi.mass_plot,dl, "nlo_plus_nll",p,K=True, logy=False, label="nlo+nll",interpolate=False)
-                plt.savefig(input.get_output_dir()+ "Kcomp_" + nlo_pdf + "_" + str(p) + ".pdf")
+                if not run_plot:
+                    dl = hepi.pdf_error(li,dl)
+                    dl = hepi.scale_error(li,dl)
+                    dl = hepi.combine_errors(dl)
+
+                    plot.data([],[],init=True)
+                    hepi.combined_plot(hepi.mass_plot,dl,"lo",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
+                    hepi.combined_plot(hepi.mass_plot,dl,"nlo",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
+                    hepi.combined_plot(hepi.mass_plot,dl,"nlo_plus_nll",p,yscale=1000,yaxis="$\sigma$ [fb]",interpolate=False)
+           
+                    plt.savefig(input.get_output_dir()+ "comp_" + nlo_pdf + "_" + str(p) + ".pdf")
 
 
-                hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + "mass" + str(p)+  "_"+ str(nlo_pdf) + ".tex")
+                    plot.data([],[],init=True)
+                    hepi.combined_plot(hepi.mass_plot,dl, "lo",p, logy=False,K=True, label="lo",interpolate=False)
+                    hepi.combined_plot(hepi.mass_plot,dl, "nlo",p, logy=False,K=True, label="nlo",interpolate=False)
+                    hepi.combined_plot(hepi.mass_plot,dl, "nlo_plus_nll",p,K=True, logy=False, label="nlo+nll",interpolate=False)
+                    plt.savefig(input.get_output_dir()+ "Kcomp_" + nlo_pdf + "_" + str(p) + ".pdf")
+
+                    hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + "mass" + str(p)+  "_"+ str(nlo_pdf) + "_" +str(scenario) + ".tex")
     wait()
