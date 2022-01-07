dc506c7fce139c558e9aee2804a2419e77c1b369
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-01-07 16:22:22 +0100

    Scale rename

--- cluster/run_mass_gaugino.py
+++ cluster/run_mass_gaugino.py
@@ -8,32 +8,32 @@ for run_plot in [True, ]:
             for ps in [2000002, 1000002]:
                 p = 1000022
                 i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, ps, p, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)
-    
-                li = hepi.mass_scan([i], p, np.linspace(900, 2000, 32))
+
+                li = hepi.mass_scan([i], p, np.linspace(1000, 2000, 20+1))
                 li = hepi.seven_point_scan(li)
                 li = hepi.pdf_scan(li)
-    
+
                 dll = rs.run(li, False, False, run_plot,False)
-    
+
                 if not run_plot:
                     dl = hepi.pdf_error(li,dl)
                     dl = hepi.scale_error(li,dl)
                     dl = hepi.combine_errors(dl)
-    
+
                     plot.data([],[],init=True)
                     hepi.combined_plot(hepi.mass_plot,dl,"lo",p,logy=True,interpolate=False)
                     hepi.combined_plot(hepi.mass_plot,dl,"nlo",p,logy=True,interpolate=False)
                     hepi.combined_plot(hepi.mass_plot,dl,"nlo_plus_nll",p,logy=True,interpolate=False)
-               
+
                     plt.savefig(input.get_output_dir()+ "comp_" + nlo_pdf + "_" + str(p) + ".pdf")
-    
-    
+
+
                     plot.data([],[],init=True)
                     hepi.combined_plot(hepi.mass_plot,dl, "lo",p, logy=False,K=True, label="lo",interpolate=False)
                     hepi.combined_plot(hepi.mass_plot,dl, "nlo",p, logy=False,K=True, label="nlo",interpolate=False)
                     hepi.combined_plot(hepi.mass_plot,dl, "nlo_plus_nll",p, logy=False,K=True, label="nlo+nll",interpolate=False)
                     plt.savefig(input.get_output_dir()+ "Kcomp_" + nlo_pdf + "_" + str(p) + ".pdf")
-    
+
                     hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + "mass" + str(p)+  "_"+ str(nlo_pdf) + "_" +str(scenario) + ".tex")
 
     #wait()
--- cluster/run_scale_plehn.py
+++ cluster/run_scale_plehn.py
@@ -14,8 +14,8 @@ for run_plot in [True,False]:
 
                 if not run_plot:
                     hepi.scale_plot(dl,["lo","nlo","nlo_plus_nll"])
-                    plt.savefig(input.get_output_dir() +"scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf")
+                    plt.savefig(input.get_output_dir() +get_job_name() + "_scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf")
                     hepi.central_scale_plot(dl,["lo","nlo","nlo_plus_nll"])
-                    plt.savefig(input.get_output_dir() +"central_scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf")
+                    plt.savefig(input.get_output_dir() +get_job_name() +"_central_scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf")
     wait()
 
