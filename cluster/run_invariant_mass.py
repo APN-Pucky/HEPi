ae623b5220a170d00a044efb8447760e27832d0f
Alexander Neuwirth <a_neuw01@uni-muenster.de>
2022-01-08 18:29:16 +0100

    inv mass run

--- cluster/run_invariant_mass.py
+++ cluster/run_invariant_mass.py
@@ -2,23 +2,28 @@ import cluster
 from cluster import *
 
 #wait()
-for run_plot in [True,False ]:
-    for scenario in ["scenarioA.in","scenarioB.in"]:
+for run_plot in [False ]:
+    for scenario in ["scenarioB.in","scenarioA.in",]:
         for lo_pdf,nlo_pdf in [("CT18NLO","CT18NLO"), ("MSHT20nlo_as118","MSHT20nlo_as118"), ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")]:
             for p in [2000002, 1000002]:
                 li = [hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=50)]
 
-                li = hepi.scan_invariant_mass(li, np.linspace(2000, 6000, 40))
+                massmin = 2000 if scenario == "scenarioB.in" else 4500
+                li = hepi.scan_invariant_mass(li, np.linspace(massmin , massmin + 4000, 40+1))
                 li = hepi.seven_point_scan(li)
 
                 dl = rs.run(li, False, False, run_plot,False)
 
                 if not run_plot:
                     dl = hepi.scale_error(li,dl)
-                    hepi.plot(dl, "invariant_mass", "lo_scale", logy=False, label="lo",yaxis="$d\\sigma/dM$ [pb/GeV]")
-                    hepi.plot(dl, "invariant_mass", "nlo_scale", logy=False, label="nlo",yaxis="$d\\sigma/dM$ [pb/GeV]")
-                    hepi.plot(dl, "invariant_mass", "nlo_plus_nll_scale", logy=False, label="nlo+nll",yaxis="$d\\sigma/dM$ [pb/GeV]")
+                    mask = dl["lo_scale"]!= np.array(None)
+                    print(dl["nlo_scale"][mask])
+                    print(dl["nlo_plus_nll_scale"][mask])
+                    plot.data([],[],init=True)
+                    hepi.plot(dl, "invariant_mass", "lo_scale", logy=False, label="lo",mask= mask,xaxis="$M$",yaxis="$d\\sigma/dM$ [pb/GeV]")
+                    hepi.plot(dl, "invariant_mass", "nlo_scale", logy=False, label="nlo",mask= mask,xaxis="$M$",yaxis="$d\\sigma/dM$ [pb/GeV]")
+                    hepi.plot(dl, "invariant_mass", "nlo_plus_nll_scale", logy=False, label="nlo+nll",mask= mask,xaxis="$M$",yaxis="$d\\sigma/dM$ [pb/GeV]")
 
-                    plt.savefig(input.get_output_dir() + "inv_mass_" + str(p) + "_" + str(nlo_pdf) + ".pdf")
+                    plt.savefig(input.get_output_dir() + "inv_mass_" + str(p) + "_" + str(nlo_pdf) + "_" +str(scenario) + ".pdf")
     wait()
 
