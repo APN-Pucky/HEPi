a192a1df9c2c9cabe211d18a529631c508719781
Alexander Neuwirth <a_neuw01@uni-muenster.de>
2022-01-17 17:12:21 +0100

    scenario based inv mass range

--- cluster/run_invariant_mass.py
+++ cluster/run_invariant_mass.py
@@ -3,7 +3,7 @@ from cluster import *
 
 #wait()
 for run_plot in [True,False ]:
-    for scenario in ["scenarioB_mod.in","scenarioA.in",]:
+    for diff_m,scenario in [(2000,"scenarioB_mod.in"),(4000,"scenarioA.in"),]:
         pdfs = [("MSHT20nlo_as118","MSHT20nlo_as118"), #("CT18NLO","CT18NLO"),  ("NNPDF40_lo_as_01180","NNPDF40_nlo_as_01180")
         ]
         for lo_pdf,nlo_pdf in pdfs:
@@ -11,7 +11,7 @@ for run_plot in [True,False ]:
                 li = [hepi.Input(hepi.Order.NLO_PLUS_NLL, 13000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=100)]
 
                 
-                li = hepi.scan_invariant_mass(li, 4000,20+1)
+                li = hepi.scan_invariant_mass(li, diff_m,20+1)
                 li = hepi.seven_point_scan(li)
 
                 dl = rs.run(li, False, False, run_plot,False)
