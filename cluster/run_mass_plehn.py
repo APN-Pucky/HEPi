53dff4ccae5c8a658345f30657bf7573e4a5d330
Alexander Neuwirth <a_neuw01@uni-muenster.de>
2022-01-18 07:48:46 +0100

    more infos

--- cluster/run_mass_plehn.py
+++ cluster/run_mass_plehn.py
@@ -6,7 +6,7 @@ for run_plot in [True,False]:
     for scenario in ["sps1a1000_mod.in"]:
         for lo_pdf,nlo_pdf in [("cteq6l1","cteq66")]:
             for p in [2000002, 1000002]:
-                i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 7000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=100)
+                i = hepi.Input(hepi.Order.NLO_PLUS_NLL, 7000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.0001,max_iters=200)
 
                 li = hepi.mass_scan([i], p, np.linspace(300, 950, 26+27), diff_L_R=20)
                 #li = hepi.seven_point_scan(li)
@@ -20,7 +20,7 @@ for run_plot in [True,False]:
                     #dl = hepi.scale_error(li,dl)
                     #dl = hepi.combine_errors(dl)
 
-                    hepi.mass_and_K_plot(dl,li,p,plehn=True,plot_data=False,figsize=(6,8),fill=False,scenario="SPS1a$_{1000}$",bbox_inches = 'tight', pad_inches = 0)
+                    hepi.mass_and_K_plot(dl,li,p,plehn=True,plot_data=True,figsize=(6,8),fill=False,scenario="SPS1a$_{1000}$",bbox_inches = 'tight', pad_inches = 0)
                     plt.savefig(input.get_output_dir()+ get_job_name()+"_comp_" + nlo_pdf + "_" + str(p) + ".pdf")
 
                     #hepi.tex_table(dl,"mass_"+str(p),input.get_output_dir() + get_job_name()+"_mass" + str(p)+  "_"+ str(nlo_pdf) + "_" +str(scenario) + ".tex")
--- cluster/run_scale_plehn.py
+++ cluster/run_scale_plehn.py
@@ -4,7 +4,9 @@ from cluster import *
 for run_plot in [True,False]:
     for scenario in ["sps1a1000_mod.in"]:
         for lo_pdf,nlo_pdf in [("cteq66","cteq66")]:
-            for p in [2000002, 1000002,1000021]:
+            parts = [2000002, 1000002,1000022,1000021
+]
+            for p in parts:
                 li = [hepi.Input(hepi.Order.NLO_PLUS_NLL, 7000, p, 1000022, scenario, lo_pdf, nlo_pdf, 1., 1.,precision=0.001,max_iters=100)]
 
                 li = hepi.scale_scan(li, 9+8,10.)
@@ -12,7 +14,7 @@ for run_plot in [True,False]:
                 dl = rs.run(li, False, False, run_plot,False)
 
                 if not run_plot:
-                    hepi.scale_plot(dl,["lo","nlo","nlo_plus_nll"],error=False)
+                    hepi.scale_plot(dl,["lo","nlo","nlo_plus_nll"],error=False,li=li,scenario="SPS1a$_{1000}$")
                     plt.savefig(input.get_output_dir() +get_job_name() + "_scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf",bbox_inches = 'tight', pad_inches = 0)
                     hepi.central_scale_plot(dl,["lo","nlo","nlo_plus_nll"])
                     plt.savefig(input.get_output_dir() +get_job_name() +"_central_scale_variation_" + str(p) + "_" + str(nlo_pdf) + "_" + str(scenario) + ".pdf",bbox_inches = 'tight', pad_inches = 0)
