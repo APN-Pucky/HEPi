e130c3ae4e264338725697fd2cb8d1bddd254d1c
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-05-18 22:34:36 +0200

    Deploying to gh-pages from @ APN-Pucky/HEPi@03a4f27455e310d5fe1b9cb0e605da4aa517d4a7 🚀

---
+++ autoapi/hepi/plot/index-1.py
@@ -0,0 +1,6 @@
+import urllib.request
+import hepi
+dl = hepi.load(urllib.request.urlopen(
+"https://raw.githubusercontent.com/fuenfundachtzig/xsec/master/json/pp13_hino_NLO%2BNLL.json"
+))
+hepi.plot(dl,"N1","NLO_PLUS_NLL",xaxis=r"$m_{       ilde{\chi}_1^0}$")
