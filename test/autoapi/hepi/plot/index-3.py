82516a47162e2bfa505d20f783d19e194e30edb4
APN-Pucky <APN-Pucky@users.noreply.github.com>
2022-12-05 12:30:19 +0100

    Deploying to gh-pages from @ APN-Pucky/HEPi@33bb368c711b7db74ef85059ef6b9f483c7f9f5e 🚀

--- test/autoapi/hepi/plot/index-3.py
+++ test/autoapi/hepi/plot/index-3.py
@@ -3,4 +3,4 @@ import hepi
 dl = hepi.load(urllib.request.urlopen(
 "https://raw.githubusercontent.com/APN-Pucky/xsec/master/json/pp13_hinosplit_N2N1_NLO%2BNLL.json"
 ),dimensions=2)
-hepi.scatterplot(dl,"N1","N2","NLO_PLUS_NLL",xaxis="$m_{\\tilde{\\chi}_1^0}$ [GeV]",yaxis="$m_{\\tilde{\\chi}_2^0}$ [GeV]" , zaxis="$\\sigma_{\\mathrm{NLO+NLL}}$ [pb]")
+hepi.scatterplot(dl,"N1","N2","NLO_PLUS_NLL_COMBINED",xaxis="$m_{\\tilde{\\chi}_1^0}$ [GeV]",yaxis="$m_{\\tilde{\\chi}_2^0}$ [GeV]" , zaxis="$\\sigma_{\\mathrm{NLO+NLL}}$ [pb]")
